59d3d3a018838914ff7be5d0f92443fee487f711
Dmitry Trager <dmitry.trager@flatstack.com>
2015-08-04 16:49:25 +0300

    pass :recall option from after_set_user hook to warden throw

--- lib/devise/hooks/activatable.rb
+++ lib/devise/hooks/activatable.rb
@@ -4,7 +4,8 @@
 Warden::Manager.after_set_user do |record, warden, options|
   if record && record.respond_to?(:active_for_authentication?) && !record.active_for_authentication?
     scope = options[:scope]
+    recall = options[:recall]
     warden.logout(scope)
-    throw :warden, scope: scope, message: record.inactive_message
+    throw :warden, scope: scope, recall: recall, message: record.inactive_message
   end
 end
